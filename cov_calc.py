Everything is working except the first two mode covariances. For some reason, these possess negative eigenvalues, and I can't see any stupid mistake. Somehow, the calculation of covariances is inducing negative eigenvalues! What's going on?

cov_calc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from numpy import linalg as LA
 from scipy.interpolate import PPoly, splrep
 from math import factorial
 from Crawlers import KCalc
@@ -116,7 +117,7 @@ radii=np.asarray([0.0,1.0,2.0])
 #radii=np.asarray([1.0,2.0,3.0,4.0])
 nradii=len(radii)
 
-nmodes=2
+nmodes=5
 
 cov_dict=dict()
 hcalc = HCalc.HCalc()
@@ -133,6 +134,12 @@ for l in range(0,nmodes):
 print cov_dict
 
 
+w,v = LA.eig(cov_dict[0])
+print w
+print v
+
+
+
 #Biasing at origin to specific value and zero gradient!
 nu=3
 bias_vals=[np.sqrt(4.0*np.pi)*nu,0.0]
@@ -146,7 +153,15 @@ for l in range(0,2):
 	s22 = cov_dict[l][1:,1:]
 
 	reduced_mean.append(s21*(1.0/s11)*bias_vals[l])
-	reduced_cov.append(s22 - np.outer(s21,s12)*(1.0/s11))
+	reduced_cov.append(s22 - np.outer(s12,s21)*(1.0/s11))
+
+#	s22 = cov_dict[l][-1,-1]
+#	s21 = cov_dict[l][0,0:-1]
+#	s12 = cov_dict[l][0:-1,0]
+#	s11 = cov_dict[l][0:-1,0:-1]
+
+#	reduced_mean.append(s12*(1.0/s22)*bias_vals[l])
+#	reduced_cov.append(s11 - np.outer(s12,s21)*(1.0/s22))
 
 
 np.savez("covariance",nmodes=nmodes, radii=radii, cov_dict=cov_dict,nu=nu,reduced_cov=reduced_cov,reduced_mean=reduced_mean)

sampler.py
@@ -2,8 +2,44 @@
 
 import numpy as np
 from numpy.random import multivariate_normal
+from numpy import linalg as LA
 
 f = np.load("covariance.npz")
 
-nradii=len(f["radii"])
+radii = f["radii"]
+nradii=len(radii)
+#sample = multivariate_normal(f["reduced "
+
+sample_dict=dict()
+
+#Sample the 00 field.
+mean = f["reduced_mean"][0]
+cov = f["reduced_cov"][0]
+
+
+w,v = LA.eig(f["cov_dict"][()][4])
+print w
+print v
+#print cov
+part1=np.sqrt(4.0*np.pi)*f["nu"]
+part2=multivariate_normal(mean,cov)
+total = np.zeros(nradii)
+total[0] = part1
+total[1:] = part2
+sample_dict[(0,0)] = total 
+
+#Sample the 1m field.
+mean = f["reduced_mean"][1]
+cov = f["reduced_cov"][1]
+for m in range(-1,2):
+	part1=0.0
+	part2=multivariate_normal(mean,cov)
+	total = np.zeros(nradii)
+	total[0] = part1
+	total[1:] = part2
+	sample_dict[(1,m)] = total 
+
+for l in range(2,f['nmodes']):
+	for m in range(-l,l+1):
+		sample_dict[(l,m)] = multivariate_normal(np.zeros(nradii),f["cov_dict"][()][l])
 