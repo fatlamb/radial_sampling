71e918f0bc8c059db43542993c25fd3981dd4a4a
Zander Moss <zander@mit.edu>
2017-05-13 17:00:06 -0400

    Committing old versions of files before formally updating to the scaled field formalism.

--- compute_covariance.py
+++ compute_covariance.py
@@ -9,21 +9,17 @@ import numpy as np
 from tqdm import tqdm
 from timeit import default_timer as timer
 
-def calc_var(l,r_alpha,spline,delta_h):
-	#Extract breakpoints from the piecewise polynomial.
-	bkpts = spline.x
-	#Extract polynomial coefficients.
-	coeffs = spline.c
+def calc_var(l,r_alpha,_bkpts,_coeffs,delta_h):
 
 	icalc = ICalc.ICalc()
 	zflag = icalc.DetZero(r_alpha)
 
 	accum = 0
-	for i in range(len(bkpts)-1):
-		accum += (coeffs[3,i] - coeffs[2,i]*bkpts[i] + coeffs[1,i]*bkpts[i]**2 - coeffs[0,i]*bkpts[i]**3)*delta_h[0,i]
-		accum += (coeffs[2,i] - 2*coeffs[1,i]*bkpts[i] + 3*coeffs[0,i]*bkpts[i]**2)*delta_h[1,i]
-		accum += (coeffs[1,i] - 3*coeffs[0,i]*bkpts[i])*delta_h[2,i]
-		accum += (coeffs[0,i])*delta_h[3,i]
+	for i in range(len(_bkpts)-1):
+		accum += (_coeffs[3,i] - _coeffs[2,i]*_bkpts[i] + _coeffs[1,i]*_bkpts[i]**2 - _coeffs[0,i]*_bkpts[i]**3)*delta_h[0,i]
+		accum += (_coeffs[2,i] - 2*_coeffs[1,i]*_bkpts[i] + 3*_coeffs[0,i]*_bkpts[i]**2)*delta_h[1,i]
+		accum += (_coeffs[1,i] - 3*_coeffs[0,i]*_bkpts[i])*delta_h[2,i]
+		accum += (_coeffs[0,i])*delta_h[3,i]
 
 	if zflag:
 		var = (16.0*np.pi**2)*2**(2*l+2)*factorial(l+1)**2/(factorial(2*(l+1)))**2*accum
@@ -35,11 +31,7 @@ def calc_var(l,r_alpha,spline,delta_h):
 
 	return var
 
-def calc_covar(l,r_alpha,r_beta,spline,delta_k):
-	#Extract breakpoints from the piecewise polynomial.
-	bkpts = spline.x
-	#Extract polynomial coefficients.
-	coeffs = spline.c
+def calc_covar(l,r_alpha,r_beta,_bkpts,_coeffs,delta_k):
 
 	icalc = ICalc.ICalc()
 	azflag = icalc.DetZero(r_alpha)
@@ -47,11 +39,11 @@ def calc_covar(l,r_alpha,r_beta,spline,delta_k):
 
 	accum = 0
 
-	for i in range(len(bkpts)-1):
-		accum += (coeffs[3,i] - coeffs[2,i]*bkpts[i] + coeffs[1,i]*bkpts[i]**2 - coeffs[0,i]*bkpts[i]**3)*delta_k[0,i]
-		accum += (coeffs[2,i] - 2*coeffs[1,i]*bkpts[i] + 3*coeffs[0,i]*bkpts[i]**2)*delta_k[1,i]
-		accum += (coeffs[1,i] - 3*coeffs[0,i]*bkpts[i])*delta_k[2,i]
-		accum += (coeffs[0,i])*delta_k[3,i]
+	for i in range(len(_bkpts)-1):
+		accum += (_coeffs[3,i] - _coeffs[2,i]*_bkpts[i] + _coeffs[1,i]*_bkpts[i]**2 - _coeffs[0,i]*_bkpts[i]**3)*delta_k[0,i]
+		accum += (_coeffs[2,i] - 2*_coeffs[1,i]*_bkpts[i] + 3*_coeffs[0,i]*_bkpts[i]**2)*delta_k[1,i]
+		accum += (_coeffs[1,i] - 3*_coeffs[0,i]*_bkpts[i])*delta_k[2,i]
+		accum += (_coeffs[0,i])*delta_k[3,i]
 	
 	if azflag and bzflag:
 		covar = (16.0*np.pi**2)*2**(2*l+2)*factorial(l+1)**2/(factorial(2*(l+1)))**2*accum
@@ -65,40 +57,33 @@ def calc_covar(l,r_alpha,r_beta,spline,delta_k):
 	return covar
 
 
-#k,p = np.loadtxt('spectrum.csv',dtype=(float,float), delimiter=",", unpack=True)
+spline_data = np.load("spline_down.npz")
+bkpts = spline_data['bkpts']
+coeffs = spline_data['coeffs']
 
-k=np.logspace(-4,3,200)
-#p=0.9*(np.exp(-20*k)+ 0.1*np.exp(-5*k))
-
-p=np.exp(-k)
-
-integral_parameters = np.load("integral_parameters.npz")
-modes = integral_parameters['modes']
+integral_parameters = np.load("output/integral_parameters0.npz")
+#modes = integral_parameters['modes']
 radii = integral_parameters['radii']
 nradii=len(radii)
 
-delta_dict = pickle.load(open("integrals.p","rb"))
+modes=range(0,9)
+print "MODES: ",modes
 
-#We may have to resample in the future to make the k and bkpts match.
-#This shouldn't cause trouble because we can compute the spectra
-#at very high resolution without too much trouble.
 
-#Fit a cubic spline to the data.
-csp = CubicSpline(k,p)
+
 
 cov_dict=dict()
-print "MODES: ",modes
 
 start=timer()
 times=[]
 #for l in tqdm(range(0,nmodes)):
 for l in tqdm(modes):
-	print "l", l
+	delta_dict = pickle.load(open("output/integrals"+str(l)+".p","rb"))
 	cov_dict[l] = np.zeros((nradii,nradii))
 	for ri1 in range(nradii):
-		(cov_dict[l])[ri1,ri1] = calc_var(l,radii[ri1],csp,delta_dict[(l,ri1,ri1)])
+		(cov_dict[l])[ri1,ri1] = calc_var(l,radii[ri1],bkpts,coeffs,delta_dict[(ri1,ri1)])
 		for ri2 in range(ri1+1,nradii):
-			(cov_dict[l])[ri1,ri2] = calc_covar(l,radii[ri1],radii[ri2],csp,delta_dict[(l,ri1,ri2)])
+			(cov_dict[l])[ri1,ri2] = calc_covar(l,radii[ri1],radii[ri2],bkpts,coeffs,delta_dict[(ri1,ri2)])
 			(cov_dict[l])[ri2,ri1] = (cov_dict[l])[ri1,ri2]
 	end=timer()
 	times.append(end-start)
@@ -108,4 +93,5 @@ print times
 print radii
 print cov_dict
 
-np.savez("covariance",modes=modes, radii=radii, cov_dict=cov_dict)
+np.savez("covariance",modes=modes, radii=radii)
+pickle.dump(cov_dict, open("covariance.p","wb"))
--- sampler.py
+++ sampler.py
@@ -1,51 +1,161 @@
 #! /usr/bin/python
-
+import matplotlib.pyplot as plt
+import pickle
 import numpy as np
+import math
 from numpy.random import multivariate_normal
 from numpy import linalg as LA
 
+cov_dict = pickle.load(open("covariance.p","rb"))
 f = np.load("covariance.npz")
+
+deriv_cov_dict = pickle.load(open("deriv_covariance.p","rb"))
+df = np.load("deriv_covariance.npz")
 
 radii = f["radii"]
 nradii=len(radii)
 #sample = multivariate_normal(f["reduced "
+#cov_dict = f["cov_dict"]
+
+#print cov_dict 
 
 sample_dict=dict()
 phi_list=[]
 
-for j in range(0,10):
+
+#Scrub negative eigenvalues
+cov_scrubbed=dict()
+
+def scrubcov(_cov):
+	w,v = LA.eig(_cov)
+	for n,el in enumerate(w):
+		if el<0:
+			w[n]=0
+	return np.dot(v,np.diag(np.sqrt(w)))
+
+for l in range(0,len(f['modes'])):
+	cov_scrubbed[l] = scrubcov(cov_dict[l])
+
+
+nu=15/math.sqrt(4*np.pi)
+bias_val=np.sqrt(4.0*np.pi)*nu
+reduced_cov = list()
+reduced_cov_scrubbed = list()
+reduced_mean = list()
+s11 = cov_dict[0][0,0]
+s12 = cov_dict[0][0,1:]
+s21 = cov_dict[0][1:,0]
+s22 = cov_dict[0][1:,1:]
+
+reduced_mean.append(s21*(1.0/s11)*bias_val)
+reduced_cov.append(s22 - np.outer(s12,s21)*(1.0/s11))
+
+
+#Gradiant Biasing
+s11 = deriv_cov_dict[-1]
+s12 = deriv_cov_dict[1]
+s21 = deriv_cov_dict[1]
+s22 = cov_dict[1]
+
+reduced_mean.append(np.zeros(nradii))
+reduced_cov.append(s22 - np.outer(s12,s21)*(1.0/s11))
+
+
+
+for i in range(0,2):
+	reduced_cov_scrubbed.append(scrubcov(reduced_cov[i]))
+
+
+for l in f['modes']:
+	negs=[]
+	#print "Mode=",l
+	w,v = LA.eig(cov_dict[l])
+	#print w
+	for x in w:
+		if x<0:
+			negs.append(x)
+	#print "ABS: ",np.max(np.abs(negs))
+#print v
+
+
+print "RAD: ", radii[0:10]
+
+print "DDVAR:",deriv_cov_dict[-1]
+print "DCOV1:",deriv_cov_dict[1]
+
+print "COV0", cov_dict[0]
+print "COV1", cov_dict[1]
+
+
+
+for j in range(0,1):
 	#Sample the 00 field.
-	mean = f["reduced_mean"][0]
-	cov = f["reduced_cov"][0]
-	part1=np.sqrt(4.0*np.pi)*f["nu"]
-	part2=multivariate_normal(mean,cov)
+	#print "bleat1"
+	mean = reduced_mean[0]
+	cov = reduced_cov_scrubbed[0]
+	#print mean.shape
+	#print cov.shape
+
+	z = multivariate_normal(np.zeros(len(mean)),np.diag(np.ones(len(mean))))
+	#print "MEAN: ",mean
+	#print "Z: ", z
+	part2 = mean + np.dot(cov,z)
+	part1=np.sqrt(4.0*np.pi)*nu
+	#print "P2: ",part2
+#	part2=multivariate_normal(mean,cov)
+	#print "bleat1"
 	total = np.zeros(nradii)
 	total[0] = part1
 	total[1:] = part2
 	sample_dict[(0,0)] = total 
+	#print "Part1: ",part1
+	#print "TOTAL: ", total
 	
 	#Sample the 1m field.
-	mean = f["reduced_mean"][1]
-	cov = f["reduced_cov"][1]
+	#print "bleat2"
+	mean = reduced_mean[1]
+	cov = reduced_cov_scrubbed[1]
 	for m in range(-1,2):
-		part1=0.0
-		part2=multivariate_normal(mean,cov)
-		total = np.zeros(nradii)
-		total[0] = part1
-		total[1:] = part2
-		sample_dict[(1,m)] = total 
-	
-	for l in range(2,f['nmodes']):
+		z = multivariate_normal(np.zeros(len(mean)),np.diag(np.ones(len(mean))))
+		sample_dict[(1,m)] = mean + np.dot(cov,z)
+#		part2 = mean + np.dot(cov,z)
+#		part1=0.0
+#		#part2=multivariate_normal(mean,cov)
+#		total = np.zeros(nradii)
+#		total[0] = part1
+#		total[1:] = part2
+#		sample_dict[(1,m)] = total 
+	#print "bleat3"
+	#for l in range(0,len(f['modes'])):
+	for l in range(2,len(f['modes'])):
+		cov=cov_scrubbed[l]
 		for m in range(-l,l+1):
-			sample_dict[(l,m)] = multivariate_normal(np.zeros(nradii),f["cov_dict"][()][l])
-	
+			z = multivariate_normal(np.zeros(nradii),np.diag(np.ones(nradii)))
+			sample_dict[(l,m)] = np.dot(cov,z)
+
 	Phi = np.zeros(nradii)
-	for l in range(0,f['nmodes']):
+	l_stack=list()
+	for l in range(0,len(f['modes'])):
+		l_stack.append(np.zeros(nradii))
+	for l in range(0,len(f['modes'])):
+	#	for l in range(0,2):
 		for m in range(-l,l+1):
 			for r in range(nradii):
-				Phi[r] += radii[r]**(2*l)*sample_dict[(l,m)][r]**2
+				#Phi[r] += radii[r]**(2*l)*sample_dict[(l,m)][r]**2
+				Phi[r] += sample_dict[(l,m)][r]**2
+				l_stack[l][r] = Phi[r]
 	phi_list.append(Phi)
 
 
-np.savez("radial_sample",radii=radii, sample_dict=sample_dict, nu=f['nu'],phi_list=phi_list)
+colors=['red','orange','yellow','blue','purple']
+
+fig,ax = plt.subplots()
+#ax.plot(radii,sample_dict[(1,0)])
+for l in range(0,5):
+	ax.plot(radii,l_stack[l],color=colors[l])
+ax.plot(radii,Phi,'k')
+#ax.set_yscale('log')
+plt.show()
+
+#np.savez("radial_sample",radii=radii, sample_dict=sample_dict, nu=nu,phi_list=phi_list)
 
