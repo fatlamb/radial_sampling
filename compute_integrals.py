d332a9613f5de1567ca7875a2487e77b7685b8ba
Zander Moss <zander@mit.edu>
2017-07-12 23:44:17 -0400

    Updated compute_integrals.py to levin method.

--- compute_integrals.py
+++ compute_integrals.py
@@ -2,42 +2,97 @@
 
 import pickle
 from scipy.interpolate import CubicSpline
-from Crawlers import KCalc
-from Crawlers import HCalc
-from Crawlers import ICalc
+from scipy import integrate as sciint
+import scipy.special as special
 import numpy as np
 from tqdm import tqdm
 from timeit import default_timer as timer
 import os
+import sys
 
+#Load target path for integral dictionaries. 
 intpath=os.environ['INTDIR']
+
+#Load working path from environment.
+pbhdir = os.environ['PBH']
+
+#Import the levin module
+sys.path.append(pbhdir+'/levin_integrals')
+import levin
 
 spline_degree=3 #Cubic splines
 
-def calc_diag_delta(l,r_alpha,bkpts):
-	hcalc = HCalc.HCalc()
-	icalc = ICalc.ICalc()
-	npoints = len(bkpts)
-	delta_h = np.zeros((spline_degree+1,npoints-1))
+def k2_analytic(a,b,alpha,beta,l):
+	"""
+	Substitutes parameters into the analytic result
+	for Integrate(k^2j_l(alpha*k)j_l(beta*k),{k,a,b})	
+	that was derived in arXiv:1703.06428
+	"""
+	res_a = (a**2/(alpha**2-beta**2))*(beta*special.spherical_jn(l,alpha*a)
+		*special.spherical_jn(l-1,beta*a) - alpha*special.spherical_jn(l-1,alpha*a)
+		*special.spherical_jn(l,beta*a))
 
+	T1 = beta*special.spherical_jn(l,alpha*a)*special.spherical_jn(l-1,beta*a)
+	T2 = -1.0*alpha*special.spherical_jn(l-1,alpha*a)*special.spherical_jn(l,beta*a)
+
+	sumsize = abs(T1)+abs(T2)
+	diffsize = abs(T1+T2)
+	if sumsize!=0:
+		print "PrecLoss: ",diffsize/sumsize
+
+	res_b = (b**2/(alpha**2-beta**2))*(beta*special.spherical_jn(l,alpha*b)
+		*special.spherical_jn(l-1,beta*b) - alpha*special.spherical_jn(l-1,alpha*b)
+		*special.spherical_jn(l,beta*b))
+	return res_b - res_a
+
+
+def k0(k):
+	return 1.0
+def k1(k):
+	return k
+def k2(k):
+	return k**2
+def k3(k):
+	return k**3
+
+poly_funcs = [k0,k1,k2,k3]
+
+integrator = levin.SphericalBesselIntegrator()
+
+def calc_relerr(err,result):
+	if err!=0.0:
+		rel_err = abs(err/result)
+	else:
+		rel_err = 0
+	return rel_err
+
+def calc_diag_delta(l,alpha,bkpts):
+	npoints = len(bkpts)
+	delta = np.zeros((spline_degree+1,npoints-1))
 	for i in range(npoints-1):
 		for n in range(spline_degree+1):
-			kpair = [bkpts[i],bkpts[i+1]]
-			delta_h[n,i] = hcalc.Calculate(kpair,l,n+2,r_alpha)
+			delta[n,i] = integrator.HCalc(bkpts[i],bkpts[i+1],alpha,l,poly_funcs[n])
+	return delta
 
-	return delta_h
-
-
-def calc_offdiag_delta(l,r_alpha,r_beta,bkpts):
-	kcalc = KCalc.KCalc()
+def calc_offdiag_delta(l,alpha,beta,bkpts):
 	npoints = len(bkpts)
-	delta_k = np.zeros((spline_degree+1,npoints-1))
-
+	delta = np.zeros((spline_degree+1,npoints-1))
+#	print "                                alpha,beta: ", alpha[0]," , ",beta[0]
 	for i in range(npoints-1):
 		for n in range(spline_degree+1):
-			kpair = [bkpts[i],bkpts[i+1]]
-			delta_k[n,i] = kcalc.Calculate(kpair,l,n+2,r_alpha,r_beta)	
-	return delta_k
+			delta[n,i] = integrator.KCalc(bkpts[i],bkpts[i+1],alpha,beta,l,poly_funcs[n])
+			#print("Numerical: ",'{:3E}'.format(delta[n,i]))
+#			if l>0 and n==2:
+#				analytic = k2_analytic(bkpts[i],bkpts[i+1],alpha[0],beta[0],l)
+#				abserr = abs(delta[n,i]-analytic)				
+#				relerr = calc_relerr(abserr,analytic)				
+#				if relerr>1e-6:
+#					print("Deviation from analytic result greater than 1e-6")
+#					print("[",bkpts[i],",",bkpts[i+1],"]")
+#					print("Analytic: ",analytic)
+#					print("AbsDev: ",abserr)
+#					print("RelDev: ",relerr)
+	return delta
 
 
 breakpoints=np.logspace(-4,3,50)
@@ -53,11 +108,11 @@ for l in tqdm(modes):
 	delta_dict=dict()
 
 	for ri1 in range(nradii):
-		delta_dict[(ri1,ri1)] = calc_diag_delta(l,radii[ri1],breakpoints)
+		delta_dict[(ri1,ri1)] = calc_diag_delta(l,[radii[ri1],ri1],breakpoints)
 		for ri2 in range(ri1+1,nradii):
-			delta_dict[(ri1,ri2)] = calc_offdiag_delta(l,radii[ri1],radii[ri2],breakpoints)
+			delta_dict[(ri1,ri2)] = calc_offdiag_delta(l,[radii[ri1],ri1],[radii[ri2],ri2],breakpoints)
 
-#	np.savez("intpath+"/integral_parameters"+str(l),bkpts=breakpoints, l=l, radii=radii)
+#	np.savez(intpath+"/integral_parameters"+str(l),bkpts=breakpoints, l=l, radii=radii)
 #	pickle.dump(delta_dict, open(intpath+"/integrals"+str(l)+".p","wb"))
 
 	end=timer()
@@ -65,5 +120,3 @@ for l in tqdm(modes):
 	start=end
 
 print times
-print radii
-
